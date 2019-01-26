542c28c33766b3e2f4cfe65a16cbb688797cea44
Chelsea Crowson <chelsea.crowson@me.com>
2019-01-25 21:13:45 -0600

    add file structure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,38 @@
+class ApplicationController < ActionController::API
+    def login
+        user = User.find_by(username: params[:username])
+
+        if user && user.authenticate(params[:password])
+            render :json => {
+                :token => JWT.encode({user_id: user.id}, nil, 'none')
+            }
+        else
+            render :json => {
+                :message => 'Invalid Credentials'
+            }, status: 400
+        end
+    end
+
+    def updateuser
+        authorization_header = request.headers['Authorization']
+
+        payload = nil
+
+        if authorization_header
+            payload = authorization_header.split(' ')[1]
+        end
+
+        if authorization_header && JWT.decode(payload, nil, false)[0]['user_id'] == params[:id].to_i
+            # if JWT is valid
+            user = User.find(params[:id])
+            user.update(uswername: params[:username])
+
+            render json: user
+        else
+            # if JWT is NOT valid
+            render :json => {
+                :message => "NOOOOOO!!!!!"
+            }, status: 400
+        end
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'login', to: 'application#login'
+  # to: chooses the controller to use and the method
+
+  post 'users/:id', to: 'application#updateuser'
+end
