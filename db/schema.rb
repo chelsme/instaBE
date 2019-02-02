a5da1958d03e0a1b4d89ef111a374b65ac7337bb
Chelsea Crowson <chelsea.crowson@me.com>
2019-02-01 23:15:10 -0600

    RemoveLikeFromLikes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_191157) do
+ActiveRecord::Schema.define(version: 2019_02_02_051424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2019_02_01_191157) do
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "post_id"
-    t.boolean "like"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
